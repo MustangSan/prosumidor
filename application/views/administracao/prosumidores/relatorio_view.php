f341865d2557180454a27ac50a79790513840a9a
luannoe <luan.noe@gmail.com>
2014-02-11 16:24:08 -0200

    Views

--- application/views/administracao/prosumidores/relatorio_view.php
+++ application/views/administracao/prosumidores/relatorio_view.php
@@ -1,43 +1,79 @@
-<?php 		
+<?php 
+	$this->load->view('pages/header.php');
 	$base = $this->config->item('base_url');
 ?>
-<html>
-<body>
 
-<?php
-	echo form_open();
+<div class="all-wrapper">
+  <div class="row">
+    <div class="col-md-3">
+      <div class="text-center">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
 
-	echo '<h3 class="form-title form-title-first"><i class="icon-shopping-cart"></i> Gerar Relatorio</h3>';
-	echo '<div class="row">';
-	
-	$data = array(
-      'name'        => 'dias',
-	  'type'		=> 'number',
-	  'class'		=> 'form-control',
-	  'value'		=> $dias
-    );			
-	echo '<div class="col-md-12"><div class="form-group"><label>Quantidade de dias</label>'.form_input($data);
-	echo form_error('dias', '<a href="#" class="fieldError" rel="tooltip" title="', '"><i class="icon-warning-sign"></i></a>');
-	echo '</div></div>';
+	  <?php $this->load->view('pages/menu-administrador.php'); ?>
+    </div>
 
-	echo'</div>';
+    <div class="col-md-9">
+      <div class="content-wrapper wood-wrapper">
+        <div class="content-inner">
+          <div class="page-header page-header-dark-blue">
+            <h1><i class="icon-user"></i> Usuários</h1>
+          </div>
 
-	$data = array(
-	  'type'		=> 'submit',
-      'name'        => 'submit',
-      'id'          => 'submit',
-      'class'       => 'btn btn-primary',
-	  'value' 		=> 'Concluir',
-	  'onclick'  	=> 'setTimeout(twoClicks, 1);'
-    );
-	echo form_input($data);
-	
-	form_close();
+          <div class="main-content">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="widget">
+                  <div class="widget-content-white glossed">
+                    <div class="padded">
+						<?php
+							echo form_open();
 
-	echo '&nbsp;<a href="'.$base.'administracao/prosumidores" class="btn btn-danger">Voltar</a>';
-?>
+							echo '<h3 class="form-title form-title-first"><i class="icon-shopping-cart"></i> Gerar Relatorio</h3>';
+							echo '<div class="row">';
+							
+							$data = array(
+						      'name'        => 'dias',
+							  'type'		=> 'number',
+							  'class'		=> 'form-control',
+							  'value'		=> $dias
+						    );			
+							echo '<div class="col-md-12"><div class="form-group"><label>Quantidade de dias</label>'.form_input($data);
+							echo form_error('dias', '<a href="#" class="fieldError" rel="tooltip" title="', '"><i class="icon-warning-sign"></i></a>');
+							echo '</div></div>';
 
-</body>
-</html>
+							echo'</div>';
 
+							$data = array(
+							  'type'		=> 'submit',
+						      'name'        => 'submit',
+						      'id'          => 'submit',
+						      'class'       => 'btn btn-primary',
+							  'value' 		=> 'Concluir',
+							  'onclick'  	=> 'setTimeout(twoClicks, 1);'
+						    );
+							echo form_input($data);
+							
+							form_close();
+
+							echo '&nbsp;<a href="'.$base.'administracao/prosumidores" class="btn btn-danger">Voltar</a>';
+						?>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php $this->load->view('pages/footer.php'); ?>
 
